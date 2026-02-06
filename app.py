3ae07c2ad86e52ca72a8bb038166b7675da624e3
pabitrapal18 <pabitrapal18aug@gmail.com>
2026-02-06 21:14:07 +0530

    Second commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import os
 
 app = Flask(__name__)
 
@@ -40,4 +41,5 @@ def index():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
